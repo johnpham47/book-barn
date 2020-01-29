12d5e781bdfc47e7580fe65547429e79e2140647
johnpham47 <johnpham47@gmail.com>
2020-01-29 13:26:51 -0600

    Added route components to app

---
+++ client/src/Components/ViewBooks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ViewBooks extends Component {
+    render() {
+        return (
+            <h1>View All Books</h1>
+        )
+    }
+}
+
+export default ViewBooks
--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Route, Switch, Link} from 'react-router-dom'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 import BaseLayout from './Components/BaseLayout'
+import ViewBooks from './Components/ViewBooks'
 
 ReactDOM.render(
 <BrowserRouter>
     <BaseLayout>
         <Switch>
             <Route exact path="/" component = {App} />
+            <Route path="/view" component = {ViewBooks} />
         </Switch>
     </BaseLayout>
 
