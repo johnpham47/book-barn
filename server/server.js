a74ab7099bd42e381ed1a02825843348c4275b7d
johnpham47 <johnpham47@gmail.com>
2020-02-03 15:22:59 -0600

    Added registration page and password hashing

--- server/server.js
+++ server/server.js
@@ -4,17 +4,46 @@ const cors = require('cors')
 const Sequelize = require('sequelize')
 const models = require('./models')
 const PORT = process.env.PORT || 8080;
+const bcrypt = require('bcrypt')
+const SALT_ROUNDS = 10
 
 app.use(express.json())
 app.use(cors())
 
 
-app.get('/foo', (req,res) => {
-    res.send('Hello')
+app.get('/displayBooks', (req,res) => {
+    models.Books.findAll().then((result) => {
+        res.json({results: result})
+    })
 })
+
+app.post('/register', (req, res) => {
+    console.log('Registering user...')
+    let username = req.body.username
+    let password = req.body.password
+
+    models.Users.findOne({
+        where: {
+            username: username
+        }
+    }).then((user) => {
+        if (user) {
+            res.status(500).json({message: 'Username already exists'})
+        }
+        else {
+            bcrypt.hash(password, SALT_ROUNDS)
+            .then(hash => {
+                models.Users.create({
+                    username: username,
+                    password: hash
+                })
+            })
+        }
+    })
+})
+
 app.post('/submitBook', (req, res) => {
     console.log("submitBook")
-    console.log(req.body)
     let title = req.body.title
     let genre = req.body.genre
     let publisher = req.body.publisher
@@ -28,6 +57,39 @@ app.post('/submitBook', (req, res) => {
         year: year,
         imgURL: imgURL
     })
+})
+
+app.post('/updateBook', (req, res) => {
+    console.log("Updating book")
+    let id = req.body.id
+    let title = req.body.title
+    let genre = req.body.genre
+    let publisher = req.body.publisher
+    let year = req.body.year
+    let imgURL = req.body.imgURL
+
+    models.Books.update({
+        title: title,
+        genre: genre,
+        publisher: publisher,
+        year: year,
+        imgURL: imgURL
+    }, {
+        where: {
+            id: id
+        }
+    }).then(res.send("Book Updated!"))
+})
+
+app.post('/deleteBook', (req, res) => {
+    console.log("Deleting book")
+    let id = req.body.id
+    models.Books.destroy({
+        where: {
+            id: id
+        }
+    })
+    
 })
 
 app.listen(8080, () => {
