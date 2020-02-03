ff90bdeb6fa7003aa940429d5d62bc4acb8c9666
johnpham47 <johnpham47@gmail.com>
2020-02-03 15:23:23 -0600

    Completed registration

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,20 +1,43 @@
 import React from 'react'
+import {useState} from 'react'
 
 const Register = () => {
+    const [user, setUser] = useState({})
+
+    function onHandleInput(e) {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function onSubmitRegistration() {
+        console.log('Registering...')
+        fetch('http://localhost:8080/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: user.username,
+                password: user.password
+            })
+        })
+    }
     return (
         <div>
             <h1>Register</h1>
             <label>Enter Username: </label>
             <br />
-            <input name="username"  type="textbox" placeholder="Enter username" />
+            <input name="username" onChange={onHandleInput} type="textbox" placeholder="Enter username" />
             <br />
             <br />
             <label>Enter Password: </label>
             <br />
-            <input name="password"  type="textbox" placeholder="Enter password" />
+            <input name="password" onChange={onHandleInput} type="textbox" placeholder="Enter password" />
             <br />
             <br />
-            <button>Register</button>
+            <button onClick={onSubmitRegistration}>Register</button>
         </div>
     )
 }
